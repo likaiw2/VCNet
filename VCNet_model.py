b5610fbe077789b5f23a5984648745069ea9a967
diode-xyt-desktop <jamesguillaumes0510@gmail.com>
2024-03-18 02:25:47 +0800

    repaired the maxpool

--- VCNet_model.py
+++ VCNet_model.py
@@ -85,10 +85,10 @@ class UNet_v2(nn.Module):
         self.down_4_conv1 = nn.Conv3d(in_channels=256, out_channels=256, kernel_size=4, dilation=1,  stride=2, padding=1)
         self.down_4_conv2 = nn.Conv3d(in_channels=128, out_channels=256, kernel_size=3, dilation=1,  stride=1, padding=1)
 
-        self.pool1 = nn.MaxPool3d(2)
-        self.pool2 = nn.MaxPool3d(2)
-        self.pool3 = nn.MaxPool3d(2)
-        self.pool4 = nn.MaxPool3d(2)
+        self.pool1 = nn.MaxPool3d(kernel_size=4, dilation=1,  stride=2, padding=1)
+        self.pool2 = nn.MaxPool3d(kernel_size=4, dilation=1,  stride=2, padding=1)
+        self.pool3 = nn.MaxPool3d(kernel_size=4, dilation=1,  stride=2, padding=1)
+        self.pool4 = nn.MaxPool3d(kernel_size=4, dilation=1,  stride=2, padding=1)
         
         # dilated conv + RB 作者表述不清不楚， 邮件询问后，得到三个 dilated RB 一模一样
         self.mid_middle1 = ResidualBlock(in_channels=256,out_channels=256)
@@ -168,11 +168,11 @@ class UNet_v2(nn.Module):
                 tools.saveRawFile10(f"{dataSavePath}/#down_8",f"testRAW_{i}",out[0, i, :, :, :])
         
         # dilated conv + RB 作者表述不清不楚，目前暂定三个 dilated RB 一模一样
-        out=self.mid_middle1(out)
+        # out=self.mid_middle1(out)
         # print("mid_1",out.shape)
-        out=self.mid_middle2(out)
+        # out=self.mid_middle2(out)
         # print("mid_2",out.shape)
-        out=self.mid_middle3(out)
+        # out=self.mid_middle3(out)
         # print("mid_3", out.shape, "\n")
         if test_mode:
             for i in range(32):
