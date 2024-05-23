2fe99c923f972feb19ba5cad5da255b1b1a2e5da
Diode-autoDL-4090D <jamesguillaumes0510@gmail.com>
2024-05-24 00:55:02 +0800

    rename model name and open wandb

--- DCGAN_new/config.py
+++ DCGAN_new/config.py
@@ -3,7 +3,7 @@ from yacs.config import CfgNode as CN
 _C = CN()
 
 _C.WANDB = CN()
-_C.WANDB.WORK = False
+_C.WANDB.WORK = True
 _C.WANDB.LOG_DIR = ""
 _C.WANDB.STATUS = "online"
 
@@ -17,7 +17,7 @@ _C.dataset.mask_type = "train"
 
 # 保存网络相关的参数
 _C.net = CN()
-_C.net.model_name = "DCGAN_ori+dilated"
+_C.net.model_name = "DCGAN_dilated+tri"
 _C.net.gen_input_channel = 1
 _C.net.gen_dp_prob = 0.2
 _C.net.disc_input_channel = 2
