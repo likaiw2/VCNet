b3a08a6595973e53b68494c043b80fb3a9225896
Diode-autoDL-4090D <jamesguillaumes0510@gmail.com>
2024-05-26 05:25:40 +0800

    run dilation

--- DCGAN_new/config.py
+++ DCGAN_new/config.py
@@ -3,7 +3,7 @@ from yacs.config import CfgNode as CN
 _C = CN()
 
 _C.WANDB = CN()
-_C.WANDB.WORK = False
+_C.WANDB.WORK = True
 _C.WANDB.LOG_DIR = ""
 _C.WANDB.STATUS = "online"
 
@@ -19,7 +19,7 @@ _C.dataset.mask_type = "train"
 # 保存网络相关的参数
 _C.net = CN()
 _C.net.pth_load_path = ""
-_C.net.model_name = "DCGAN_ori"
+_C.net.model_name = "DCGAN_dila"
 # _C.net.model_name = "pix2pix"
 _C.net.gen_input_channel = 1
 _C.net.gen_dp_prob = 0.2
@@ -28,7 +28,7 @@ _C.net.learning_rate = 0.0002             #原模型参数 5e-3(0.005)
 _C.net.batch_size = 1
 _C.net.lambda_recon = 200
 
-_C.net.dilation_flag = False
+_C.net.dilation_flag = True
 _C.net.trilinear_flag = False
 _C.net.partial_flag = False
 _C.net.smooth_L1_flag = False
